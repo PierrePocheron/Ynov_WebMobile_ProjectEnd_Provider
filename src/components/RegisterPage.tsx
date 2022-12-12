838d6c241be44f32702327878b96d27bf166bc81
PierrePocheron <pierre.pocheron@gmail.com>
2022-12-12 09:50:25 +0100

    feat: create register page

---
+++ src/components/RegisterPage.tsx
@@ -0,0 +1,105 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { collection, addDoc } from 'firebase/firestore';
+import { useReducer, useState } from 'react';
+import { auth, db } from '../main';
+
+
+const RegisterPage = () => {
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [professionalIdentifier, setProfessionalIdentifier] = useState<string>("");
+
+  const handleClickRegister = async () => {
+      const userCreate = await createUserWithEmailAndPassword(auth, email, password)
+        .catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+        })
+
+      if (userCreate){
+        const userCreateDoc = addDoc(collection(db, 'users'), {
+          uid: userCreate.user.uid,
+          type: "PROVIDER",
+          professionalIdentifier: professionalIdentifier
+        })
+      }
+      // if(userCreateDoc) Redirect to /login
+  }
+
+  const handleChangeEmail = (event: any) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const handleChangePassword = (event: any) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const handleChangeProfessionalIdentifier = (event: any) => {
+    setProfessionalIdentifier(event.currentTarget.value)
+  }
+
+  return (
+    <main className="main">
+      <div className='container'>
+        <section className="wrapper-Regsiter">
+          <div className="form">
+            <h1 className="text text-large">Provider without account ? Register 👀</h1>
+            <p className="text text-normal">You have account ?
+              <span>
+                <a href="login" className="text text-links">Login</a>
+              </span>
+            </p>
+            <div className="input-control">
+              <label htmlFor="username" hidden>Username</label>
+              <input
+              type="text"
+              id="username"
+              name="username"
+              className="input-field"
+              value={email}
+              onChange={handleChangeEmail}
+              placeholder="Email Address">
+              </input>
+            </div>
+
+            <div className="input-control">
+              <label htmlFor="password" hidden>Password</label>
+              <input type="password"
+              id="password"
+              name="password"
+              className="input-field"
+              value={password}
+              onChange={handleChangePassword}
+              placeholder="Password">
+              </input>
+            </div>
+
+            <div className="input-control">
+              <label htmlFor="professionalIdentifier" hidden>Professional identifier</label>
+              <input type="professionalIdentifier"
+              id="professionalIdentifier"
+              name="professionalIdentifier"
+              className="input-field"
+              value={professionalIdentifier}
+              onChange={handleChangeProfessionalIdentifier}
+              placeholder="Professional identifier">
+              </input>
+            </div>
+
+            <button className="input-submit" onClick={handleClickRegister}>Regsiter</button>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default RegisterPage;
