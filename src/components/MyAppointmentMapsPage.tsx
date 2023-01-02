99b7e23b15456160708fc38fef34d5502498b2c1
PierrePocheron <pierre.pocheron@gmail.com>
2023-01-02 12:15:18 +0100

    feat: add my appointments maps page & redirection

--- src/components/MyAppointmentMapsPage.tsx
+++ src/components/MyAppointmentMapsPage.tsx
@@ -9,66 +9,13 @@ const MyAppointmentMapsPage = () => {
 
   const navigate = useNavigate();
   const currentProvider = auth.currentUser
-  const [appointmentTime, setAppointmentTime] = useState<string>("");
-  const [listAppointment, setListAppointment] = useState<any[]>();
-
 
   useEffect(() => {
     if (!auth.currentUser){
       navigate("/login")
     }
-
-    const fetchData = async () => {
-      const qAppointment = query(collection(db, 'appointments'),
-                where('provider', '==', currentProvider?.uid),
-                where('status', '==', 'WAITING_CONFIRMATION')
-                );
-      const getAppointments = await getDocs(qAppointment)
-
-      const listAppointmentsTemp = await Promise.all(
-        getAppointments.docs.map( async (appointment) => {
-
-          var appointmentData = appointment.data()
-          console.log(appointmentData.patient)
-
-          const qPatient = query(collection(db, 'users'), where('uid', '==', appointmentData.patient));
-          const getPatient = await getDocs(qPatient)
-
-          var patientData = getPatient.docs.at(0)?.data()
-
-          return ({
-            id: appointment.id,
-            data: appointmentData,
-            patient: patientData,
-          })
-        })
-      )
-      setListAppointment(listAppointmentsTemp)
-    }
-    fetchData();
   }, [])
 
-
-  const handleClickChooseTimeAppointment = async (event:any, appointmentId:string) => {
-    const appointmentRef = doc(db, "appointments", appointmentId);
-
-    const data = {
-      time: appointmentTime,
-      status: 'CONFIRMED'
-    };
-
-    updateDoc(appointmentRef, data)
-    .then(docRef => {
-        console.log("Time set for appointment");
-    })
-    .catch(error => {
-        console.log(error);
-    })
-  }
-
-  const handleChangeTime = (event: any) => {
-    setAppointmentTime(event.currentTarget.value)
-  }
 
   const handleClickButtonMyAppointment = (event: any) => {
     navigate("/my-appointment");
